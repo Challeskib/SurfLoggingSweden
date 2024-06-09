38c9d7ae632bb94b141db703643cd7df18aba2e6
Charlie Skibsted <charlieskibsted@gmail.com>
2024-06-09 18:45:29 +0200

    Small changes

---
+++ SurfLoggingSweden.Shared/Models/SurfSessionDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SurfLoggingSweden.Shared.Models
+{
+    public class SurfSessionDto
+    {
+        public int Id { get; set; }
+        public int SurfSpotId { get; set; }
+        public string SurfSpotName { get; set; }
+        public int WindDegree { get; set; }
+        public int Rating { get; set; }
+        public int WindPower { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
